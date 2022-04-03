4577914c93d9b8838673de0ce92dba935066b137
elizabethmgorman <elizabethmargaretgorman@gmail.com>
2022-04-03 16:08:15 -0500

    CSS commit

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask
-from flask import render_template
+from cmath import pi
+from flask import Flask, redirect
+from flask import render_template, redirect, request, url_for
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance ')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
+
+@app.route('/estimate', methods=['GET''POST'])
+def estimate():
+    return render_template('estimate.html', pageTitle='Make Estimate')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
