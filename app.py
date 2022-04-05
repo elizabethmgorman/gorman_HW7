d85859e4bedb9cb1baa73972f70d89c53a188cce
elizabethmgorman <elizabethmargaretgorman@gmail.com>
2022-04-04 19:58:14 -0500

    APP Update

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ def index():
 def about():
     return render_template('about.html', pageTitle='About Vertical Tank Maintenance')
 
-@app.route('/estimate', methods=['POST','GET'])
+@app.route('/estimate', methods=['GET''POST'])
 def estimate():
+total_estimate= " "
     if request.method=="POST":
-        radius= float(request.form['tank_radius'])
-        height= float(request.form['tank_height'])
-        area_tank_top= pi*radius**2
-        area_sides=2*(pi*(radius*height))
+        radius= request.form['tank_weight']
+        height= request.form['tank_height']
+        area_tank_top= pi* radius^2
+        area_sides=2*(pi(radius*height))
         total_area= area_tank_top + area_sides
         square_feet= total_area/144
         material_cost= square_feet*25
         labor_cost= square_feet*15
         total_estimate= material_cost+ labor_cost
-        total_estimate = str(round(total_estimate, 2))
-        return render_template('estimate.html', pageTitle='Make Estimate', estimate = total_estimate)
+        return redirect(url_for('estimate'))
     return render_template('estimate.html', pageTitle='Make Estimate')
 
 
